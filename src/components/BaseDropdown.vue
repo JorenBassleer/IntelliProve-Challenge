3233e94271956cb6be4a6b75df63c089c95f539b
Joren Bassleer <jorenbassleer1997@gmail.com>
2025-03-04 22:46:02 +0100

    add dropdown

---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,15 @@
+<template>
+  <dropdown-menu>
+    <template #trigger>
+      <slot />
+    </template>
+
+    <template #body>
+      <slot name="content" />
+    </template>
+  </dropdown-menu>
+</template>
+<script setup>
+import DropdownMenu from 'v-dropdown-menu';
+
+</script>
--- src/components/UserSelect.vue
+++ src/components/UserSelect.vue
@@ -1,22 +1,35 @@
 <template>
-  <BaseIcon
-    icon-name="pi-user"
-    class="pi pi-user text-xl text-primary-hover cursor-pointer"
-  />
-  <article
-    v-for="user in externalUserIds"
-    :key="user"
-    @click="store.authenticate(user)"
-  >
-    {{ user }}
-  </article>
+  <BaseDropdown>
+    <BaseIcon
+      icon-name="pi-user"
+      class="pi pi-user text-xl text-primary-hover cursor-pointer select-none"
+    />
+    <template #content>
+      <section class="bg-white rounded p-2 border border-color shadow-xl">
+        <article
+          v-for="user in externalUserIds"
+          :key="user"
+          class="cursor-pointer text-primary hover:text-primary-hover transition-colors duration-300"
+          @click="store.authenticate(user)"
+        >
+          {{ user }}
+        </article>
+      </section>
+    </template>
+  </BaseDropdown>
 </template>
 <script setup>
 import { storeToRefs } from 'pinia';
 import BaseIcon from './BaseIcon.vue';
 import { useBaseStore } from '../store/base';
+import BaseDropdown from './BaseDropdown.vue';
 
 const store = useBaseStore();
 
 const { externalUserIds } = storeToRefs(store);
 </script>
+<style scoped>
+.border-color {
+  border-color: rgba(9, 33, 135, 0.07);
+}
+</style>
